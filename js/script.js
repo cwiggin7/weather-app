d0f81ee2f1a67f8ad78c6238726cc2542aa43ba9
cwiggin7 <cwiggin7@ksu.edu>
2023-07-17 18:17:10 -0500

    Reworked user interface.

--- js/script.js
+++ js/script.js
@@ -1,73 +1,88 @@
-import apiKey from "../../config/secrets.js";
+/* eslint-disable linebreak-style */
+/* eslint-disable camelcase */
 
-const searchBar = document.getElementById("search-bar");
-const conditionElement = document.getElementById("condition");
-const cityRegionElement = document.getElementById("city-region");
-const temperatureElement = document.getElementById("temperature");
-const feelsLikeElement = document.getElementById("feels-like");
-const windMPHElement = document.getElementById("wind-mph");
+const apiKey = "07ad528bf198493aaa615753231207";
+
+const searchBarElement = document.getElementById("search-bar");
+
+const conditionElement = document.getElementById("weather-condition");
+const locationElement = document.getElementById("location");
+const temperatureElement = document.getElementById("temperature-fahrenheit");
+
+const feelsLikeElement = document.getElementById("feels-like-fahrenheit");
 const humidityElement = document.getElementById("humidity");
+const windElement = document.getElementById("wind-mph");
+
+function updateWeatherUI(weatherData) {
+  const { text, feelslike_f, humidity, temp_f, wind_mph, name, region } =
+    weatherData;
+
+  conditionElement.innerText = text;
+  locationElement.innerText = `${name.toUpperCase()}, ${region.toUpperCase()}`;
+  temperatureElement.innerText = Math.round(temp_f);
+
+  feelsLikeElement.innerText = Math.round(feelslike_f);
+  humidityElement.innerText = humidity;
+  windElement.innerText = wind_mph;
+}
 
 function weatherFactory(
-  city,
-  region,
-  temperatureFahrenheit,
-  condition,
-  feelsLikeFahrenheit,
+  text,
+  feelslike_f,
   humidity,
-  windSpeedMph,
+  temp_f,
+  wind_mph,
+  name,
+  region,
 ) {
   return {
-    city,
+    text,
+    feelslike_f,
+    humidity,
+    temp_f,
+    wind_mph,
+    name,
     region,
-    temperatureFahrenheit: Math.round(temperatureFahrenheit),
-    condition,
-    feelsLikeFahrenheit: Math.round(feelsLikeFahrenheit),
-    humidity: Math.round(humidity),
-    windSpeedMph: Math.round(windSpeedMph),
   };
 }
 
-function fetchWeather(city) {
-  const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
-
-  fetch(apiUrl)
+function fetchWeather(location) {
+  fetch(
+    `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}&days=7`,
+    { mode: "cors" },
+  )
     .then((response) => response.json())
     .then((response) => {
-      const { name } = response.location;
-      const { region } = response.location;
-
-      const { text } = response.current.condition;
-      const { temp_f } = response.current;
-      const { feelslike_f } = response.current;
-      const { humidity } = response.current;
-      const { wind_mph } = response.current;
-      const weatherData = weatherFactory(
-        name,
-        region,
+      const {
+        condition: { text },
+        feelslike_f,
+        humidity,
         temp_f,
+        wind_mph,
+      } = response.current;
+
+      const { name, region } = response.location;
+
+      const weatherData = weatherFactory(
         text,
         feelslike_f,
         humidity,
+        temp_f,
         wind_mph,
+        name,
+        region,
       );
 
-      conditionElement.innerText = weatherData.condition;
-      cityRegionElement.innerText = `${weatherData.city.toUpperCase()}, ${weatherData.region.toUpperCase()}`;
-      temperatureElement.innerText = weatherData.temperatureFahrenheit;
-      feelsLikeElement.innerText = `FEELS LIKE: ${weatherData.feelsLikeFahrenheit}`;
-      windMPHElement.innerText = `WIND: ${weatherData.windSpeedMph} MPH`;
-      humidityElement.innerText = `HUMIDITY: ${weatherData.humidity}%`;
+      updateWeatherUI(weatherData);
     });
 }
 
-function submitForm(event) {
+function handleWeatherSearch(event) {
   event.preventDefault();
-  fetchWeather(searchBar.value);
+  fetchWeather(searchBarElement.value);
+  searchBarElement.value = "";
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  fetchWeather("Kansas City");
-  document.querySelector("form").addEventListener("submit", submitForm);
-  document.querySelector("a").addEventListener("click", submitForm);
+  fetchWeather("Kansas");
 });
